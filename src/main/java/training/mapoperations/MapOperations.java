d3c235691f3f21595278719f829dbf5c012a890d
mmociewicz <mmociewicz@pl.sii.eu>
2018-06-08 00:02:57 +0200

    Added collections behaviour experiments.

--- src/main/java/training/mapoperations/MapOperations.java
+++ src/main/java/training/mapoperations/MapOperations.java
@@ -3,6 +3,7 @@ package training.mapoperations;
 import datagenerator.model.person.Person;
 import training.java8streams.approach1.FileUtil;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -19,15 +20,19 @@ public class MapOperations {
 
         System.out.println(people.size());
 
-        for(int i = 0; i <  people.size(); i++){
-            String s = "xyzzzz"+i;
+        for (int i = 0; i < people.size(); i++) {
+            String s = "xyzzzz" + i;
             personMap.put(s, people.get(i));
         }
 
         System.out.println(personMap.size());
         System.out.println(personMap.entrySet());
 
-        personMap.entrySet().stream().collect(Collectors.toList()).forEach(x ->{ System.out.println(x);});
+
+
+        personMap.entrySet().stream().collect(Collectors.toList()).forEach(x -> {
+            System.out.println(x);
+        });
 
         personMap.entrySet().stream()
                 .map(x -> x.getValue())
@@ -40,7 +45,62 @@ public class MapOperations {
         // mapa ma w key hashcode. Jeżeli typem key jest integer to jest to ten integer,
         // jeżeli typem jest np. string, to key jest również intigerem (wyliczony hashcode z tego
         // Stringa)
-        new ArrayList<>(personMap.keySet()).forEach(x -> System.out.println(x.hashCode()));
+        new ArrayList<>(personMap.keySet()).forEach(x -> System.out.println(x));
+
+        System.out.println("ARRAY LIST ADD/REMOVE");
+        List<Person> personList = new ArrayList<>(new FileUtil().getTasksModel());
+        Person person = personList.get(0);
+        System.out.println("before person from 0 " + person.getName());
+        System.out.println("before add person to 10, [10] " + personList.get(10).getName());
+        System.out.println("before add person to 10, [11] " + personList.get(11).getName());
+        System.out.println("before add person to 10, [12] " + personList.get(12).getName());
+        personList.add(10, person);
+        System.out.println("after add person to 10, [10] " + personList.get(10).getName());
+        System.out.println("after add person to 10, [11] " + personList.get(11).getName());
+        System.out.println("after add person to 10, [12] " + personList.get(12).getName());
+        personList.remove(10);
+        System.out.println("after remove person from 10, [10] " + personList.get(10).getName());
+        System.out.println("after remove person from 10, [11] " + personList.get(11).getName());
+        System.out.println("after remove person from 10, [12] " + personList.get(12).getName());
+        System.out.println("after person List size " + personList.size());
+        personList.set(12, person);
+        System.out.println("after person set at 12, [12] " + personList.get(12).getName());
+
+
+        System.out.println("LINKED LIST ADD/REMOVE");
+
+        List<Person> personList2 = new LinkedList<>(new FileUtil().getTasksModel());
+        Person person2 = personList2.get(0);
+        System.out.println("before person from 0 " + person2.getName());
+        System.out.println("before add person to 10, [10] " + personList2.get(10).getName());
+        System.out.println("before add person to 10, [11] " + personList2.get(11).getName());
+        System.out.println("before add person to 10, [12] " + personList2.get(12).getName());
+        personList2.add(10, person);
+        System.out.println("after add person to 10, [10] " + personList2.get(10).getName());
+        System.out.println("after add person to 10, [11] " + personList2.get(11).getName());
+        System.out.println("after add person to 10, [12] " + personList2.get(12).getName());
+        personList2.remove(10);
+        System.out.println("after remove person from 10, [10] " + personList2.get(10).getName());
+        System.out.println("after remove person from 10, [11] " + personList2.get(11).getName());
+        System.out.println("after remove person from 10, [12] " + personList2.get(12).getName());
+        System.out.println("after person List size " + personList2.size());
+
+        personList.set(12, person);
+        System.out.println("after person set at 12, [12] " + personList.get(12).getName());
+
+        Set<Person> personSet = new HashSet<>(personList2);
+        System.out.println("person set size: " + personSet.size());
+
+        personSet.contains(person2);
+
+        for (Person p : personSet){
+            p.getName();
+        }
+
+        personMap.putIfAbsent("xyz", person);
+
+
+
 
 
     }
