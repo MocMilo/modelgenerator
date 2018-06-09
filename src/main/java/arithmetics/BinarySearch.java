64b12a7c89e2a3cf2b6e9a24182dbf3ce4d4f681
mmociewicz <mmociewicz@pl.sii.eu>
2018-06-09 08:44:48 +0200

    in progress

--- src/main/java/arithmetics/BinarySearch.java
+++ src/main/java/arithmetics/BinarySearch.java
@@ -3,22 +3,46 @@ package arithmetics;
 public class BinarySearch {
 
     public static void main(String[] args) {
-
-
-
-
+        int[] table = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(search(table, 3));
     }
 
+    public static int search(int[] table, int searchedItem) {
+        int startIndex = 0;
+        int endIndex = table.length-1;
+        int initMiddleIndex = endIndex / 2;
 
-    public static int search(int[] table, int searchedItem){
+        if (table.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (searchedItem > table.length) {
+            throw new IllegalArgumentException();
+        }
+        if (searchedItem == initMiddleIndex) {
+            return initMiddleIndex;
+        }
 
-
-
-
-
-
-        return 0;
+        int escape = 0;
+        while (initMiddleIndex != searchedItem) {
+            int dynamicMiddleIndex = (endIndex - startIndex) / 2;
+            if (searchedItem < dynamicMiddleIndex) {
+                endIndex = dynamicMiddleIndex;
+                if (searchedItem == dynamicMiddleIndex)
+                    return searchedItem;
+            }
+            if (searchedItem > dynamicMiddleIndex) {
+                startIndex = dynamicMiddleIndex;
+                endIndex = table.length;
+                if (searchedItem == startIndex)
+                    return searchedItem;
+            }
+            escape++;
+            if (escape > 100) {
+                System.out.println("infinite loop escape");
+                return -1;
+            }
+        }
+        System.out.println("algorithm failure");
+        return -1;
     }
-
-
 }
--- src/main/java/arithmetics/BinarySearchTest.java
+++ src/main/java/arithmetics/BinarySearchTest.java
@@ -8,14 +8,36 @@ import static org.junit.Assert.*;
 public class BinarySearchTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldIfTableEmpty() {
+    public void shouldThrowExceptionIfTableEmpty() {
+        int[] table = {};
+        BinarySearch.search(table, 3);
+    }
 
-        int[] table = {1,2,3,4,5,6,7,8};
-        int searchedValue = BinarySearch.search(table, 3);
-        assertThat(3, equalTo(searchedValue) );
+    @Test
+    public void shouldNotThrowExceptionIfTableNotEmpty() {
+        int[] table = {1};
+        BinarySearch.search(table, 3);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void shouldThrowExceptionIfElementOutOfBound() {
+        int[] table = {1,2,3};
+        BinarySearch.search(table, 4);
+    }
+
+    @Test
+    public void shouldNotThrowExceptionIfElementOutOfBound() {
+        int[] table = {1,2,3};
+        BinarySearch.search(table, 3);
     }
 
 
+   /* @Test
+    public void shouldIfTableEmpty() {
+        int[] table = {1,2,3,4,5,6,7,8};
+        int searchedValue = BinarySearch.search(table, 3);
+        assertThat(3, equalTo(searchedValue) );
+    }*/
 
 
 }
