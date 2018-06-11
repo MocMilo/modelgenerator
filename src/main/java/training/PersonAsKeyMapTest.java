707521c91965ab1c2f548bee0b72b2cf84993233
mmociewicz <mmociewicz@pl.sii.eu>
2018-06-11 16:01:39 +0200

    Person as key in map example.

---
+++ src/main/java/training/PersonAsKeyMapTest.java
@@ -0,0 +1,72 @@
+package training;
+
+import training.designpatterns.simplefactory.generators.PricingGenerator;
+import training.designpatterns.simplefactory.product.Product;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class PersonAsKeyMapTest {
+
+    public static void main(String[] args) {
+
+        // Map as class idea extension
+/*
+        Map<Class<? extends Product>, Supplier<PricingGenerator>> map = new HashMap<>();
+        map.put(new Product() {}, () -> (PricingGenerator) () -> null);*/
+
+        // public getGenerator(Product p){
+        // map.get(p.getClass()).get();       //
+        // }
+
+        // Person as Key
+
+        Map<PersonAsKey, String> test = new HashMap<>();
+        PersonAsKeyMapTest personAsKeyMapTest = new PersonAsKeyMapTest();
+
+        PersonAsKey personAsKey = personAsKeyMapTest.new PersonAsKey();
+        personAsKey.age = 25;
+
+        System.out.println(personAsKey.hashCode());
+
+        test.put(personAsKey, "test");
+
+        personAsKey.age = 26;
+
+        System.out.println("hashcode after set new age:" + personAsKey.hashCode());
+
+        // personAsKey.age = 25;
+
+        System.out.println("string form map by Person: " + test.get(personAsKey));  // person is null
+        // not found
+        // hashcode
+
+    }
+
+    private class PersonAsKey {
+/*     final String name ="";
+       final int age = 0;*/
+
+        String name = "";
+        int age = 0;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PersonAsKey that = (PersonAsKey) o;
+            return age == that.age &&
+                    Objects.equals(name, that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age);
+        }
+    }
+
+
+}
